489d805601fd8c183a4cad8c507426043572a5cf
Eduard Bissell <edd.bissell@hotmail.co.uk>
2023-11-30 22:19:16 +0000

    remove: questions

--- 2021/3/answer.js
+++
@@ -1,9 +0,0 @@
-import { exampleData } from "./puzzleData.js";
-import { puzzleData } from "./puzzleData.js";
-
-let useExampleData;
-useExampleData = true;
-
-const inputData = useExampleData && exampleData ? exampleData : puzzleData;
-
-console.log(inputData);
