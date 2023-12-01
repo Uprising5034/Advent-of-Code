e2a100b3d1193764f32a80f7b35b9841ecab8874
Eduard Bissell <edd.bissell@hotmail.co.uk>
2023-12-01 11:48:27 +0000

    solved part 1

--- 2023/1/answer.js
+++ 2023/1/answer.js
@@ -3,8 +3,28 @@ import { exampleData } from "./puzzleData.js";
 import { puzzleData } from "./puzzleData.js";
 
 let useExampleData;
-useExampleData = true;
+// useExampleData = true;
 
 const inputData = useExampleData && exampleData ? exampleData : puzzleData;
 
-console.log(inputData);
+const inputArray = inputData.split("\n");
+
+function makeNumArray(inputArray) {
+  const fullArray = inputArray.map((string) => {
+    const strArray = string.split("");
+    const numArray = strArray
+      .filter((char) => !isNaN(Number(char)))
+
+    const num1 = numArray[0];
+    const num2 = numArray[numArray.length - 1];
+
+    return Number(num1 + num2);
+  });
+
+  const filterNaN = fullArray.filter((num) => !isNaN(num));
+  return filterNaN
+}
+
+const answer = makeNumArray(inputArray).reduce((a, b) => a + b, 0);
+
+console.log('answer', answer)
