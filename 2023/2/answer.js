0048948257a79b3e13905cb9ebb798822068b573
Eduard Bissell <edd.bissell@hotmail.co.uk>
2023-12-02 09:38:25 +0000

    load: 2023 2

---
+++ 2023/2/answer.js
@@ -0,0 +1,10 @@
+// https://www.adventofcode.com/2023/day/2
+import { puzzleData } from "./puzzleData.js";
+import { exampleDataPart1, exampleDataPart2 } from "./puzzleData.js";
+
+const allData = [puzzleData, exampleDataPart1, exampleDataPart2];
+
+const inputData = allData[0];
+
+console.log(inputData);
+
