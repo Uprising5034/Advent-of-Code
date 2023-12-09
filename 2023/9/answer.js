f7f8622cfc30447b089fbf24a1b8c498e400038c
Eduard Bissell <edd.bissell@hotmail.co.uk>
2023-12-09 16:01:54 +0000

    solve part 1 && part 2

---
+++ 2023/9/answer.js
@@ -0,0 +1,70 @@
+// https://www.adventofcode.com/2023/day/9
+import { puzzleData } from "./puzzleData.js";
+import { exampleDataPart1, exampleDataPart2 } from "./puzzleData.js";
+
+const dataArg = process.argv[2];
+
+const allData = [puzzleData, exampleDataPart1, exampleDataPart2];
+const input = allData[dataArg || 0].split("\n").slice(0, -1);
+
+function parse(input) {
+  return input.map((line) => line.split(" ").map((num) => Number(num)));
+}
+
+function calc(oasis, part2) {
+  const history = oasis.map((sequence) => {
+    const layers = [sequence];
+
+    let lastLayer = layers[layers.length - 1];
+    while (lastLayer.some((value) => value !== 0)) {
+      const nextLayer = [];
+      lastLayer.forEach((num, numidx) => {
+        const nextNum = lastLayer[numidx + 1];
+
+        if (!isNaN(nextNum)) {
+          nextLayer.push(nextNum - num);
+        } else if(nextLayer.length === 0) {
+          nextLayer.push(0)
+        }
+      });
+      layers.push(nextLayer);
+      lastLayer = layers[layers.length - 1];
+    }
+    return layers;
+  });
+
+  history.forEach((report) => {
+    for (let i = report.length - 2; i > -1; i--) {
+      const sequence = report[i];
+      const calcSequence = report[i + 1];
+
+      const value = part2 ? sequence[0] : sequence[sequence.length - 1];
+      const calcValue = part2 ? calcSequence[0] : calcSequence[calcSequence.length - 1];
+
+      const nextValue = part2 ? value - calcValue : value + calcValue;
+
+      if (part2) {
+        sequence.unshift(nextValue)
+      } else {
+        sequence.push(nextValue)
+      }
+    }
+  });
+
+  return history.reduce((a, b) => {
+    return part2 ? a + b[0][0] : a + b[0][b[0].length - 1];
+  }, 0);
+}
+
+function solve(input) {
+  // const oasis = parse([input[198]]);
+  const oasis = parse(input);
+
+  const answer1 = calc(oasis);
+  const answer2 = calc(oasis, true)
+
+  console.log("answer1", answer1);
+  console.log('answer2', answer2)
+}
+
+solve(input);
